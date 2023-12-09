e8730f8e1d238b4289ee2940a1c192e20456c64d
MungaiS <129229232+MungaiS@users.noreply.github.com>
2023-12-09 16:08:22 +0300

    First complete css

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-const likelyToRain = true;
-likelyToRain ? "rain boots" : "sun hat";
+
